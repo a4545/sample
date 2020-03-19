d404b4e3f7567d428e18f0b10418641402c00377
Aditya Raj <aditya.raj9901@gmail.com>
2020-03-19 17:17:31 +0530

    first commit

---
+++ data/get-blog-posts.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const META = /export\s+const\s+meta\s+=\s+(\{(\n|.)*?\n\})/;
+const DIR = path.join(process.cwd(), './pages/blog/');
+const files = fs
+    .readdirSync(DIR)
+    .filter((file) => file.endsWith('.md'));
+module.exports = files
+    .map(file => {
+        const name = path.join(DIR, file);
+        const contents = fs.readFileSync(name, 'utf8');
+
+        // const match = META.exec(contents);
+        // if (!match || typeof match[1] !== 'string')
+        //     throw new Error(`${name} needs to export const meta = {}`);
+        // const meta = eval('(' + match[1] + ')');
+        return {
+            contents,
+            path: '/blog/' + file.replace(/\.mdx?$/, '')
+        };
+    })
--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const { ApolloServer, gql } = require("apollo-server");
-
+const faker = require('faker');
+const fs = require('fs');
+const data = fs.readFileSync('./pages/data.json', 'utf8')
+const dataone = require('./data/get-blog-posts.js');
+console.log(dataone);
+const ParsedData = JSON.parse(data);
+console.log(ParsedData)
+let ID = ParsedData.allCats.map(r => {
+  return r.name;
+});
+console.log(ID);
 const typeDefs = gql`
+type Node {
+  id: ID!
+  parent: Node!
+  children: [Node!]!
+  internal: Internal!
+}
+type Internal {
+  type: String!
+}
+  type Cat{
+    id:ID!
+    name:String!
+    age:Int!
+    nice:Boolean
+  }
   type Query {
-    hello: String
+    allCats: [Cat!]!
   }
 `;
 
-const resolvers = {
-  Query: {
-    hello: () => "Hello World!"
-  }
-};
+const mocks = {
+  ID: () => faker.random.uuid(),
+  Int: () => faker.random.number({ min: 1, max: 10 }),
+  String: () => faker.name.firstName(),
+  Boolean: () => faker.random.boolean()
+}
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  mocks
 });
 
 server.listen().then(({ url }) => console.log(`Server Running on port ${url}`));
